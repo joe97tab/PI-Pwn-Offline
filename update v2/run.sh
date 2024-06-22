879db73405a583e9587692391ffb27d6d11d2b6d
joe97tab <joe97tab@gmail.com>
2024-06-23 00:13:53 +0700

    23062024 stooged updated

---
+++ update v2/run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ -f /boot/firmware/PPPwn/config.sh ]; then
+source /boot/firmware/PPPwn/config.sh
+fi
+if [ -f /boot/firmware/PPPwn/pconfig.sh ]; then
+source /boot/firmware/PPPwn/pconfig.sh
+fi
+
+if [ -z $INTERFACE ]; then INTERFACE="eth0"; fi
+if [ -z $FIRMWAREVERSION ]; then FIRMWAREVERSION="11.00"; fi
+if [ -z $USBETHERNET ]; then USBETHERNET=false; fi
+
+if [ -z $XFWAP ]; then XFWAP="1"; fi
+if [ -z $XFGD ]; then XFGD="4"; fi
+if [ -z $XFBS ]; then XFBS="0"; fi
+if [ -z $XFNWB ]; then XFNWB=false; fi
+
+sudo mkdir /boot/firmware/update/
+sudo mv /boot/firmware/PPPwn/PPPwn.tar /boot/firmware/update/PPPwn.tar
+sudo rm -rf /boot/firmware/PPPwn/
+sudo mkdir /boot/firmware/PPPwn/
+sudo tar -xf /boot/firmware/update/PPPwn.tar -C /boot/firmware/PPPwn/
+sudo chmod 777 /boot/firmware/PPPwn/*.*
+sudo rm -rf /boot/firmware/update/
+
+# creat general config
+echo '#!/bin/bash
+INTERFACE="'$INTERFACE'"
+FIRMWAREVERSION="'$FIRMWAREVERSION'"
+USBETHERNET='$USBETHERNET'' | sudo tee /boot/firmware/PPPwn/config.sh
+
+# create pppwn c++ config
+echo '#!/bin/bash
+XFWAP="'$XFWAP'"
+XFGD="'$XFGD'"
+XFBS="'$XFBS'"
+XFNWB='$XFNWB'' | sudo tee /boot/firmware/PPPwn/pconfig.sh
+
+coproc read -t 2 && wait "$!" || true
+sudo reboot
